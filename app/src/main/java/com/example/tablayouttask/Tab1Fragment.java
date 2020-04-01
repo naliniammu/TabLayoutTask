01b2a3f284702b54096dc7fa9316fb4310e10556
Puchakayala Nalini <pucnalin@publicisgroupe.net>
2020-04-01 12:13:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/tablayouttask/Tab1Fragment.java
@@ -0,0 +1,29 @@
+package com.example.tablayouttask;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Tab1Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab1, container, false);
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
